c59c59f4e2f3ea0919c272258074196e923dd026
Abhishek Jain <abhishek.jain2692@gmail.com>
2016-12-22 22:51:36 +0530

    Removed all the comments and now reading files from this.files inside task.
    - Added .gitignore file.

--- tasks/lib/process-file.js
+++ tasks/lib/process-file.js
@@ -6,13 +6,18 @@ var localModulesMap = {};
 var externModulesMap = {};
 var processedFiles = [];
 var modulePaths = {};
-// var foundStatement = [];
 var importRegEx = /^\s*import\s+\{(.*)\}\s+from\s+[\"\'](.*)[\"\'];*$/gm;
+
+exports.clean = function() {
+    localModulesMap = {};
+    externModulesMap = {};
+    processedFiles = [];
+    modulePaths = {};
+}
 
 exports.process = function (src, filepath, dest) {
 
     var match;
-    // var finalSrc = "";
     var finalSrc = src;
 
     var index = filepath.lastIndexOf("/") + 1;
@@ -21,44 +26,6 @@ exports.process = function (src, filepath, dest) {
 
     processedFiles.push(fileName.substr(0, fileName.lastIndexOf(".")).trim());
 
-    console.log("processed files: ", processedFiles);
-    console.log("paths of files: ", path);
-
-    // while ((match = regEx.exec(src)) !== null) {
-
-    //     finalSrc2 = finalSrc2.replace(match[0], "");
-
-    //     var finalMatch = match[1].trim();
-
-    //     foundStatement.forEach(function (val) {
-    //         finalMatch = finalMatch.split(",")
-    //             .map(function (str) {
-    //                 return str.trim();
-    //             })
-    //             .filter(function (str) {
-    //                 return val !== str;
-    //             })
-    //             .reduce(function (final, str) {
-    //                 if (final === "") {
-    //                     return str;
-    //                 }
-    //                 return final + ", " + str;
-    //             }, "");
-    //     });
-
-    //     if (finalMatch !== "") {
-    //         finalSrc += "import { " + finalMatch + " }" + match[2] + "\n";
-    //         finalMatch.split(",").forEach(function (newVal) {
-    //             foundStatement.push(newVal.trim());
-    //         });
-    //     }
-    // }
-
-    // return {
-    //     imports: finalSrc,
-    //     src: finalSrc2
-    // }
-
     while ((match = importRegEx.exec(src)) !== null) {
 
         finalSrc = finalSrc.replace(match[0], "");
@@ -66,18 +33,16 @@ exports.process = function (src, filepath, dest) {
         var allImports = match[1].split(",").map(function (str) {
             return str.trim();
         });
-        console.log("allImports: " + allImports);
 
         if (match[2].charAt(0) === ".") {
 
             let module = match[2].substr(match[2].lastIndexOf("/") + 1);
-            console.log("module: " + module);
+
             if (!localModulesMap[module]) {
                 localModulesMap[module] = [];
             }
 
             localModulesMap[module] = _.union(localModulesMap[module], allImports);
-            console.log("local modules: ", localModulesMap);
 
             if (!modulePaths[module]) {
 
@@ -112,18 +77,15 @@ exports.process = function (src, filepath, dest) {
                 if(modulePath[0] !== ".") modulePath = "./" + modulePath;
                 
                 modulePaths[module] = modulePath;
-                console.log("module paths: ", modulePaths);
             }
 
         } else {
 
-            console.log("match: " + match[2]);
             if (!externModulesMap[match[2]]) {
                 externModulesMap[match[2]] = [];
             }
 
             externModulesMap[match[2]] = _.union(externModulesMap[match[2]], allImports);
-            console.log("external modules: ", externModulesMap);
 
         }
     }
@@ -132,14 +94,11 @@ exports.process = function (src, filepath, dest) {
 }
 
 exports.removeDuplicates = function () {
-
     Object.keys(localModulesMap).forEach(function (key) {
         if (processedFiles.indexOf(key) >= 0) {
             delete localModulesMap[key];
         }
     });
-
-    console.log("final local modules: ", localModulesMap);
 }
 
 exports.returnImports = function () {
--- tasks/ts-concat.js
+++ tasks/ts-concat.js
@@ -10,26 +10,41 @@ module.exports = function (grunt) {
 
     grunt.registerMultiTask("ts-concat", "Concatenate typescript files gracefully", function () {
 
-        console.log(this.filesSrc);
+        this.files.forEach(function (file) {
 
-        this.filesSrc.forEach(function (filepath) {
+            if (grunt.file.exists(file.dest)) {
+                grunt.file.delete(file.dest);
+            }
 
-            var src = grunt.file.read(filepath);
+            finalImports = "";
+            finalMainSrc = "";
 
-            var finalSrc = util.process(src, filepath, this.data.dest);
-            finalMainSrc += finalSrc + "\n";
-            // finalImports += data.imports + "\n";
-            // finalMainSrc += data.src + "\n";
+            file.src
+                .filter(function (filepath) {
+                    if (grunt.file.exists(filepath)) {
+                        return true;
+                    }
+                    return false;
+                })
+                .forEach(function (filepath) {
+
+                    var src = grunt.file.read(filepath);
+
+                    var finalSrc = util.process(src, filepath, this.data.dest);
+                    finalMainSrc += finalSrc + "\n";
+
+                }, this);
+
+            util.removeDuplicates();
+
+            finalImports = util.returnImports();
+
+            grunt.file.write(file.dest, (finalImports + finalMainSrc).replace(cleanRegEx, ""));
+
+            util.clean();
 
         }, this);
 
-        util.removeDuplicates();
-        finalImports = util.returnImports();
-
-        if (grunt.file.exists(this.data.dest)) {
-            grunt.file.delete(this.data.dest);
-        }
-        grunt.file.write(this.data.dest, (finalImports + finalMainSrc).replace(cleanRegEx, ""));
     });
 
 }
